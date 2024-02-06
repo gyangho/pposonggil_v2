e6de647e04bc2d77f1cd3eafacfbb76fee1416dc
Gonagi <kkh10002@naver.com>
2024-02-06 19:16:53 +0900

    let a 추가

--- Docker/server/app.js
+++ Docker/server/app.js
@@ -165,6 +165,7 @@ app.get("/main/mypage", (req, res) => {
 app.get("/main/mypage/bookmark", async (req, res) => {
   const filePath = path.join(__dirname, "/views/bookmark.html");
   let Routes = {};
+  let a = 1;
   res.send("수정예정");
   // try {
   //   Routes = await transport.getPublicTransport(126.9961, 37.5035, 126.96, 37.4946, 202307291200); //신반포역->정보관
