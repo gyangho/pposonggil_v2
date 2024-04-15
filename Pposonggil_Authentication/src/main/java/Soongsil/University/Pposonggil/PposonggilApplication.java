966459f037244399f301e238b0fd5cd3ef3511c8
gyangho <dydtkwk@naver.com>
2024-04-15 19:16:24 +0900

    2024.04.15. 이경호 Spring 인증서버 구현 시작

---
+++ Pposonggil_Authentication/src/main/java/Soongsil/University/Pposonggil/PposonggilApplication.java
@@ -0,0 +1,13 @@
+package Soongsil.University.Pposonggil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PposonggilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PposonggilApplication.class, args);
+	}
+
+}
---
+++ Pposonggil_Authentication/src/test/java/Soongsil/University/Pposonggil/PposonggilApplicationTests.java
@@ -0,0 +1,13 @@
+package Soongsil.University.Pposonggil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PposonggilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
