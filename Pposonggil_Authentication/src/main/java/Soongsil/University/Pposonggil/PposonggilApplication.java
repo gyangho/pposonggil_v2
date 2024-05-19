dcc2707ee883ad1a5e161ddb0ec8d84fcc86e14e
gyangho <52252845+gyangho@users.noreply.github.com>
2024-05-19 15:05:25 +0900

    Revert "05.19. 이경호 아직 실행되지는 않는.. 코드"
    This reverts commit 67125882d0fcc7672bbd28b94c3f1b5ed511b277.

--- Pposonggil_Authentication/src/main/java/Soongsil/University/Pposonggil/PposonggilApplication.java
+++ Pposonggil_Authentication/src/main/java/Soongsil/University/Pposonggil/PposonggilApplication.java
@@ -2,10 +2,6 @@ package Soongsil.University.Pposonggil;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
-import org.springframework.security.web.SecurityFilterChain;
 
 @SpringBootApplication
 public class PposonggilApplication {
