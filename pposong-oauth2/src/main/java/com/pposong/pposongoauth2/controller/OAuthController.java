224aefbeb9fe851b4ab173c5072dfffc068a501b
gyangho <52252845+gyangho@users.noreply.github.com>
2024-05-25 19:59:47 +0900

    05.25. SecurityConfig 수정 및 예외 처리 추가 jwt 추가 전

--- pposong-oauth2/src/main/java/com/pposong/pposongoauth2/controller/OAuthController.java
+++ pposong-oauth2/src/main/java/com/pposong/pposongoauth2/controller/OAuthController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class OAuthController {
 
-    @GetMapping("/loginForm")
+    @GetMapping("/auth/login")
     public String home(){
         return "loginForm";
     }
