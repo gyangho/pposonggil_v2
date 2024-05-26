16a7b39110bb9599b4611e58f1156dc3a29cc43b
gyangho <52252845+gyangho@users.noreply.github.com>
2024-05-27 03:21:14 +0900

    05.27. 이경호 jwt 실패!!

---
+++ pposong-oauth2/src/main/java/com/pposong/pposongoauth2/config/JwtAuthorizationFilter.java
@@ -0,0 +1,61 @@
+package com.pposong.pposongoauth2.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.io.IOException;
+
+public class JwtAuthorizationFilter extends UsernamePasswordAuthenticationFilter {
+    private final AuthenticationManager authenticationManager;
+    private final JwtTokenProvider jwtTokenProvider;
+
+    public JwtAuthorizationFilter(AuthenticationManager authenticationManager, JwtTokenProvider jwtTokenProvider) {
+        this.authenticationManager = authenticationManager;
+        this.jwtTokenProvider = jwtTokenProvider;
+
+        setFilterProcessesUrl("/api/member/login");
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
+            throws AuthenticationException {
+        log.info("USERNAMEPASSWORD_FILTER");
+        ObjectMapper om = new ObjectMapper();
+        MemberLogin memberLogin = null;
+
+        try {
+            memberLogin = om.readValue(request.getInputStream(), MemberLogin.class);
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+        log.info("member : {}", memberLogin);
+        UsernamePasswordAuthenticationToken authenticationToken =
+                new UsernamePasswordAuthenticationToken(memberLogin.getUsername(), memberLogin.getPassword());
+
+        Authentication authentication = authenticationManager.authenticate(authenticationToken);
+
+        return authentication;
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
+                                            Authentication authResult) throws IOException, ServletException {
+
+        PrincipalDetails principalDetails = (PrincipalDetails) authResult.getPrincipal();
+
+        String jwtToken = jwtTokenProvider.generateToken(principalDetails.getUsername());
+
+        response.getWriter().write("Bearer " + jwtToken);
+        response.getWriter().flush();
+    }
+
+}
+}
