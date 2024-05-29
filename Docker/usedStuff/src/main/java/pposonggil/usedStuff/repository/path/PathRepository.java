b340df2bd32bf9fa13d5524f3471c00484fd3c03
Gonagi <kkh10002@naver.com>
2024-05-29 18:06:35 +0900

    feat: 누락한 리포지트로 코드 추가

--- Docker/usedStuff/src/main/java/pposonggil/usedStuff/repository/path/PathRepository.java
+++ Docker/usedStuff/src/main/java/pposonggil/usedStuff/repository/path/PathRepository.java
@@ -1,7 +1,7 @@
 package pposonggil.usedStuff.repository.path;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import pposonggil.usedStuff.domain.Path;
+import pposonggil.usedStuff.domain.Route.Path;
 import pposonggil.usedStuff.repository.path.custom.CustomPathRepository;
 
 public interface PathRepository extends JpaRepository<Path, Long>, CustomPathRepository {
--- Docker/usedStuff/src/main/java/pposonggil/usedStuff/repository/point/PointRepository.java
+++ Docker/usedStuff/src/main/java/pposonggil/usedStuff/repository/point/PointRepository.java
@@ -1,7 +1,7 @@
 package pposonggil.usedStuff.repository.point;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import pposonggil.usedStuff.domain.Point;
+import pposonggil.usedStuff.domain.Route.Point;
 import pposonggil.usedStuff.repository.point.custom.CustomPointRepository;
 
 public interface PointRepository extends JpaRepository<Point, Long>, CustomPointRepository {
