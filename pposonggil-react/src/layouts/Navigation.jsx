4004c578ffd1c769c21d9921826ed0e1041443d5
OOO <66418642+aayeong@users.noreply.github.com>
2024-06-03 13:08:27 +0900

    상세경로 pathId-> index로 수정 전 commit

--- pposonggil-react/src/layouts/Navigation.jsx
+++ pposonggil-react/src/layouts/Navigation.jsx
@@ -7,71 +7,6 @@ import { useRecoilState } from "recoil";
 import { navState } from "../recoil/atoms";
 import { Link } from 'react-router-dom';
 
-const Nav = styled.div`
-  outline: none;
-  display: flex;
-  flex-direction: column;
-  position: fixed;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  width: 100%;
-  height: 70px;
-  background-color: white;
-  z-index: 1000;
-  box-shadow: 0px -2px 4px rgba(0, 0, 0, 0.1);
-`;
-
-const Items = styled.ul`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 100%;
-  height: 70px;
-  background-color: white;
-  padding: 0px 5px;
-  position: relative;
-`;
-
-const Item = styled(motion.li)`
-  width: 20%;
-  height: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  cursor: pointer;
-  margin: 0px 5px;
-  position: relative;
-`;
-
-const NavIcon = styled(motion.div)`
-  font-size: 25px;
-  color: ${(props) => (props.isActive ? "#003E5E" : "#4a4a4a")};
-  display: flex;
-  flex-direction: column; /* 수직 정렬 */
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-  width: 100%;
-  height: 100%;
-  padding-top: 8px;
-  div {
-    margin-top: 8px;
-    font-size: 12px;
-    font-weight: 400;
-    color: #636363;
-  }
-`;
-
-const NavStateBar = styled(motion.div)`
-  width: 100%;
-  height: 4px;
-  background-color: #003E5E;
-  /* background-color: #41bbf8; */
-  position: absolute;
-  top: 0px;
-`;
-
 function Navigation() {
   const [nav, setNav] = useRecoilState(navState);
   const [activeIndex, setActiveIndex] = useState(0);
@@ -118,4 +53,70 @@ function Navigation() {
   );
 }
 
-export default Navigation;
+export default Navigation
+
+
+const Nav = styled.div`
+  outline: none;
+  display: flex;
+  flex-direction: column;
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  width: 100%;
+  height: 70px;
+  background-color: white;
+  z-index: 1000;
+  box-shadow: 0px -2px 4px rgba(0, 0, 0, 0.1);
+`;
+
+const Items = styled.ul`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 70px;
+  background-color: white;
+  padding: 0px 5px;
+  position: relative;
+`;
+
+const Item = styled(motion.li)`
+  width: 20%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  margin: 0px 5px;
+  position: relative;
+`;
+
+const NavIcon = styled(motion.div)`
+  font-size: 25px;
+  color: ${(props) => (props.isActive ? "#003E5E" : "gray")};
+  display: flex;
+  flex-direction: column; /* 수직 정렬 */
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  width: 100%;
+  height: 100%;
+  padding-top: 8px;
+  div {
+    margin-top: 8px;
+    font-size: 12px;
+    font-weight: 400;
+    color: inherit;
+  }
+`;
+
+const NavStateBar = styled(motion.div)`
+  width: 100%;
+  height: 4px;
+  background-color: #003E5E;
+  /* background-color: #41bbf8; */
+  position: absolute;
+  top: 0px;
+`;
