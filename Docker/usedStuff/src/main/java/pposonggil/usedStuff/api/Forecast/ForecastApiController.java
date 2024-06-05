7bb7029e9daca54a92cc7f0c8817b3fd2e447663
Gonagi <kkh10002@naver.com>
2024-06-05 14:48:22 +0900

    feat: cors 기상정보 조회 @RequestBody 삭제
    - ForecastService 내에서 현재 시각 체크

--- Docker/usedStuff/src/main/java/pposonggil/usedStuff/api/Forecast/ForecastApiController.java
+++ Docker/usedStuff/src/main/java/pposonggil/usedStuff/api/Forecast/ForecastApiController.java
@@ -2,6 +2,7 @@ package pposonggil.usedStuff.api.Forecast;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import pposonggil.usedStuff.dto.Forecast.ForecastDto;
@@ -21,8 +22,8 @@ public class ForecastApiController {
      * @return 시간으로 groupedby된 기상 정보 Dto 리스트
      */
     @GetMapping("/api/forecasts")
-    public Map<String, List<ForecastDto>> forecastsByTime(@RequestBody ForecastDto forecastDto) {
-        return forecastService.getForecastsByTime(forecastDto);
+    public Map<String, List<ForecastDto>> forecastsByTime() {
+        return forecastService.getForecastsByTime();
     }
 
     /**
@@ -30,7 +31,7 @@ public class ForecastApiController {
      * @param forecastDto
      * @return x, y가 일치하는 기상 정보 Dto 리스트
      */
-    @GetMapping("/api/forecast/by-x-y")
+    @PostMapping("/api/forecast/by-x-y")
     public List<ForecastDto> forecastsByXAndY(@RequestBody ForecastDto forecastDto) {
         return forecastService.findForecastsByXAndY(forecastDto);
     }
@@ -40,7 +41,7 @@ public class ForecastApiController {
      * @param pointInformationDto
      * @return 입력한 위도, 경도의 기상 정보 Dto 리스트
      */
-    @GetMapping("/api/forecast/by-lat-lon")
+    @PostMapping("/api/forecast/by-lat-lon")
     public List<ForecastDto> findForecastsByLatAndLon(@RequestBody PointInformationDto pointInformationDto) {
         return forecastService.findForecastsByLatAndLon(pointInformationDto);
     }
@@ -50,7 +51,7 @@ public class ForecastApiController {
      * @param forecastDto
      * @return 날짜, 시간, x, y가 일치하는 기상 정보 Dto
      */
-    @GetMapping("/api/forecast/by-time-x-y")
+    @PostMapping("/api/forecast/by-time-x-y")
     public ForecastDto forecastDtoByTimeAndXAndY(@RequestBody ForecastDto forecastDto) {
         return forecastService.findForecastByTimeAndXAndY(forecastDto);
     }
